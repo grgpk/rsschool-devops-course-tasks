f2a1cc570fa8ce3e59254585a1f87019ee19474e
Giorgi <grgpaikidze@gmail.com>
2024-10-13 16:00:45 +0400

    feat: add terraform config for creating vpc

---
+++ private-subnets.tf
@@ -0,0 +1,21 @@
+# Private Subnet 1
+resource "aws_subnet" "private_subnet_1" {
+  vpc_id            = aws_vpc.demo_vpc.id
+  cidr_block        = "10.0.3.0/24"
+  availability_zone = "us-east-1a"
+
+  tags = {
+    Name = "private-subnet"
+  }
+}
+
+# Private Subnet 2
+resource "aws_subnet" "private_subnet_2" {
+  vpc_id            = aws_vpc.demo_vpc.id
+  cidr_block        = "10.0.4.0/24"
+  availability_zone = "us-east-1b"
+
+  tags = {
+    Name = "private-subnet"
+  }
+}
---
+++ routes.tf
@@ -0,0 +1,44 @@
+# Public Route Table
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.demo_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "public-rt"
+  }
+}
+
+# Private Route Table (no internet access for private subnets)
+resource "aws_route_table" "private_rt" {
+  vpc_id = aws_vpc.demo_vpc.id
+
+  tags = {
+    Name = "private-rt"
+  }
+}
+
+# Associate public subnets with public route table
+resource "aws_route_table_association" "public_subnet_1_assoc" {
+  subnet_id      = aws_subnet.public_subnet_1.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+resource "aws_route_table_association" "public_subnet_2_assoc" {
+  subnet_id      = aws_subnet.public_subnet_2.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+# Associate private subnets with private route table
+resource "aws_route_table_association" "private_subnet_1_assoc" {
+  subnet_id      = aws_subnet.private_subnet_1.id
+  route_table_id = aws_route_table.private_rt.id
+}
+
+resource "aws_route_table_association" "private_subnet_2_assoc" {
+  subnet_id      = aws_subnet.private_subnet_2.id
+  route_table_id = aws_route_table.private_rt.id
+}
