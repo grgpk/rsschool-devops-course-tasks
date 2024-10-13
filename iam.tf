d2d1b48b28c7e6c77f6fe3d1b469c622ef7af809
Giorgi <grgpaikidze@gmail.com>
2024-10-13 16:39:11 +0400

    feat: format aim.tf file

--- iam.tf
+++ iam.tf
@@ -26,11 +26,11 @@ resource "aws_iam_role" "GithubActionsRole" {
 
   managed_policy_arns = [
     "arn:aws:iam::aws:policy/AmazonS3FullAccess",
-    "arn:aws:iam::aws:policy/AmazonEC2FullAccess"
-    "arn:aws:iam::aws:policy/AmazonRoute53FullAccess"
-    "arn:aws:iam::aws:policy/IAMFullAccess"
-    "arn:aws:iam::aws:policy/AmazonVPCFullAccess"
-    "arn:aws:iam::aws:policy/AmazonSQSFullAccess"
-    "arn:aws:iam::aws:policy/AmazonEventBridgeFullAccess"
+    "arn:aws:iam::aws:policy/AmazonEC2FullAccess",
+    "arn:aws:iam::aws:policy/AmazonRoute53FullAccess",
+    "arn:aws:iam::aws:policy/IAMFullAccess",
+    "arn:aws:iam::aws:policy/AmazonVPCFullAccess",
+    "arn:aws:iam::aws:policy/AmazonSQSFullAccess",
+    "arn:aws:iam::aws:policy/AmazonEventBridgeFullAccess",
   ]
 }
