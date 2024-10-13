6ee2b036a53c17695cf1f62e8db81352d9a21a56
Giorgi <grgpaikidze@gmail.com>
2024-10-13 16:27:42 +0400

    feat: iam role creation

---
+++ iam.tf
@@ -0,0 +1,36 @@
+resource "aws_iam_role" "GithubActionsRole" {
+  name = "GithubActionsRole"
+
+  assume_role_policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Effect" : "Allow",
+        "Action" : "sts:AssumeRoleWithWebIdentity",
+        "Principal" : {
+          "Federated" : var.aws_oidc_provider
+        },
+        "Condition" : {
+          "StringEquals" : {
+            "token.actions.githubusercontent.com:aud" : "sts.amazonaws.com",
+          },
+
+          "StringLike" : {
+            "token.actions.githubusercontent.com:sub" : "repo:grgpk/rsschool-devops-course-tasks:*",
+
+          }
+        }
+      }
+    ]
+  })
+
+  managed_policy_arns = [
+    "arn:aws:iam::aws:policy/AmazonS3FullAccess",
+    "arn:aws:iam::aws:policy/AmazonEC2FullAccess"
+    "arn:aws:iam::aws:policy/AmazonRoute53FullAccess"
+    "arn:aws:iam::aws:policy/IAMFullAccess"
+    "arn:aws:iam::aws:policy/AmazonVPCFullAccess"
+    "arn:aws:iam::aws:policy/AmazonSQSFullAccess"
+    "arn:aws:iam::aws:policy/AmazonEventBridgeFullAccess"
+  ]
+}
