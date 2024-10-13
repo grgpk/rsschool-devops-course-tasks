c53afe3bbadd5a251662e62997d0d7a708d1dcae
Giorgi <grgpaikidze@gmail.com>
2024-10-13 16:53:40 +0400

    feat: update az

--- private-subnets.tf
+++ private-subnets.tf
@@ -2,7 +2,7 @@
 resource "aws_subnet" "private_subnet_1" {
   vpc_id            = aws_vpc.demo_vpc.id
   cidr_block        = "10.0.3.0/24"
-  availability_zone = "us-east-1a"
+  availability_zone = "ueu-central-1a"
 
   tags = {
     Name = "private-subnet"
@@ -13,7 +13,7 @@ resource "aws_subnet" "private_subnet_1" {
 resource "aws_subnet" "private_subnet_2" {
   vpc_id            = aws_vpc.demo_vpc.id
   cidr_block        = "10.0.4.0/24"
-  availability_zone = "us-east-1b"
+  availability_zone = "eu-central-1b"
 
   tags = {
     Name = "private-subnet"
--- public-subnets.tf
+++ public-subnets.tf
@@ -2,7 +2,7 @@
 resource "aws_subnet" "public_subnet_1" {
   vpc_id            = aws_vpc.demo_vpc.id
   cidr_block        = "10.0.1.0/24"
-  availability_zone = "us-east-1a"
+  availability_zone = "eu-central-1a"
 
   tags = {
     Name = "public-subnet"
@@ -13,7 +13,7 @@ resource "aws_subnet" "public_subnet_1" {
 resource "aws_subnet" "public_subnet_2" {
   vpc_id            = aws_vpc.demo_vpc.id
   cidr_block        = "10.0.2.0/24"
-  availability_zone = "us-east-1b"
+  availability_zone = "eu-central-1b"
 
   tags = {
     Name = "public-subnet"
